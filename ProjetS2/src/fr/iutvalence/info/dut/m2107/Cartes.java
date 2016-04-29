8e3c6360d493d17465effb8a0eceed071ffdc53e
canseva <canseva@PC-A104-15.iut-valence.fr>
2016-04-29 15:00:11 +0200

    The class Cartes is updated , CarteCaisseDeCommunauté and CarteChance are created

--- ProjetS2/src/fr/iutvalence/info/dut/m2107/Cartes.java
+++ ProjetS2/src/fr/iutvalence/info/dut/m2107/Cartes.java
@@ -1,6 +1,64 @@
 package fr.iutvalence.info.dut.m2107;
 
+/**Cards of a Monopoly Game
+ * @author canseva
+ *
+ */
 public class Cartes
 {
 
+/**Attribute which represents the type of a card
+ * 
+ */
+private String type;
+
+/**
+ * The text written on the card
+ */
+private String texte;
+
+/**
+ * Value of the card
+ */
+private int valeur;
+
+/**
+ * Using to get the type of a card
+ * @return the type of a card
+ */
+
+public String getType()
+{
+	return type;
 }
+
+
+/**
+ * Using to get the text of a card
+ * @return the text of a card
+ */
+public String getTexte()
+{
+	return texte;
+}
+
+
+/**
+ * Using to get the value of a card
+ * @return the value of a card
+ */
+public int getValeur()
+{
+	return valeur;
+}
+
+
+
+
+
+}
+
+
+
+
+

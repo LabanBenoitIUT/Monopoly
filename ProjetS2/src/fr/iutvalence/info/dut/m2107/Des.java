ea3578e0d2198c546a67243ad0be7bb922681edd
Dylan <dylan28berard@gmail.com>
2016-05-11 10:39:13 +0200

    Creation of class and method of 'Des'

--- ProjetS2/src/fr/iutvalence/info/dut/m2107/Des.java
+++ ProjetS2/src/fr/iutvalence/info/dut/m2107/Des.java
@@ -1,6 +1,51 @@
 package fr.iutvalence.info.dut.m2107;
 
 public class Des
-{
+{	
+	/**
+	 * The value of a dice
+	 */
+	private static int valeurDe;
+	
+	
+	/**
+	 * Used to get a random number between 1 and 6
+	 * @return valeurDe
+	 */
+	public static int lancerDe(){
+
+		valeurDe = 0;
+
+		valeurDe = (int)(1+ 6*Math.random());
+
+		return valeurDe;
+
+		}
+	
+	/**
+	 * Used to compare the values of two dices
+	 * @param de1
+	 * @param de2
+	 * @return false if de1 != de2
+	 * @return true if de1=de2
+	 */
+	public static boolean isDouble(int de1, int de2){
+		
+		if(de1==de2)
+		{
+			return true;
+		}
+		
+		else
+			return false;
+		}
+
+	/**
+	 * Used to get the value of a dice
+	 * @return valeurDe
+	 */
+	public static int getValeurDe() {
+		return valeurDe;
+	}
 
 }

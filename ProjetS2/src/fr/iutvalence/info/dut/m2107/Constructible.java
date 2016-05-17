91c91197abf46cbef6fe3d6c18925f557274d1ce
canseva <canseva@PC-B102-03.iut-valence.fr>
2016-05-17 14:10:36 +0200

    constructible and casePropriete are commented

---
+++ ProjetS2/src/fr/iutvalence/info/dut/m2107/Constructible.java
@@ -0,0 +1,31 @@
+package fr.iutvalence.info.dut.m2107;
+
+public class Constructible extends CasePropriete
+{
+/**
+ * the price of a building case
+ */
+private int prixConstruction;
+/***
+ * number of building cases
+ */
+private int nbConstruction;
+
+/**
+ * used to get the price of a building case
+ * @return the price of the building case
+ */
+public int getPrixConstruction()
+{
+	return prixConstruction;
+}
+/**
+ * used to get the number of building cases
+ * @return number of building cases 
+ */
+public int getNbConstruction()
+{
+	return nbConstruction;
+}
+
+}

d4b342054e2c022888211921619ee1d147877473
veyrea <veyrea@PC-A101-11.iut-valence.fr>
2016-05-20 13:57:48 +0200

    test graphic

--- ProjetS2/src/fr/iutvalence/info/dut/m2107/Main.java
+++ ProjetS2/src/fr/iutvalence/info/dut/m2107/Main.java
@@ -2,6 +2,9 @@ package fr.iutvalence.info.dut.m2107;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 /**
  * application who execute a Monopoly
  * @author laban benoit
@@ -13,12 +16,20 @@ public class Main
 	 * begin the game
 	 * @param args
 	 */
+	@SuppressWarnings("null")
 	public static void main(String[] args)
 	{
+		
 		new Partie().Play();
-		System.out.println();
 		JFrame fenetre = new JFrame();
-		fenetre.setVisible(true);
-	}
+		fenetre.setTitle("Monopoly");
+	    Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+	    int height = (int)dimension.getHeight();
+	    int width  = (int)dimension.getWidth();
+	    fenetre.setSize(width, height);
+	    fenetre.setLocationRelativeTo(null);
+	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    fenetre.setVisible(true);
+	 }
 
 }

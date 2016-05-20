6c73683458255b80602600f3361ee1463ae24bb9
veyrea <veyrea@PC-B122-08.iut-valence.fr>
2016-05-20 10:27:57 +0200

    test Jframe

--- ProjetS2/src/fr/iutvalence/info/dut/m2107/CasePrison.java
+++ ProjetS2/src/fr/iutvalence/info/dut/m2107/CasePrison.java
@@ -10,7 +10,7 @@ public class CasePrison extends Cases
 	 * getPrisonniers: get the name of the prisoners 
 	 * @return the name of prisoners 
 	 */
-	public Joueur getPrisonniers()
+	//public Joueur getPrisonniers()
 	{
 		
 	}
